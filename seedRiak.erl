45697c6363772b391a0b5edfa27f321fe7e4547c
Alex Harb <alex.harb11@gmail.com>
2019-03-01 22:36:21 -0800

    Finished with erlang experiments

--- seedRiak.erl
+++ seedRiak.erl
@@ -1,21 +1,28 @@
-% hello world program
 -module(seedRiak). 
--export([for/2,start/0]). 
+% -export([for/1,start/0]). 
+-export([start/0]). 
 
-for(0,_) -> 
-   []; 
-   
-
-  for(N,Term) when N > 0 -> 
-   X = integer_to_list(N),
-   Y = unicode:characters_to_list(["./jsonData/file", X, ".json"], latin1),
-   io:fwrite("~p~n", [Y]), 
-  [Term|for(N-1,Term)]. 
+  % for(N) when N =< 10 -> 
+  %  X = integer_to_list(N),
+  %  Y = unicode:characters_to_list(["./jsonData/file", X, ".json"], latin1),
+  %  io:fwrite("~p~n",[Y]),
+  % for(N+1).
 
 start() -> 
-  for(10, 1).
+  % for(0).
 
+{ok, Pid} = riakc_pb_socket:start("127.0.0.1", 8087),
 
-%{ok, File} = file:open("./jsonData/fileN.json", [read]),
-%Txt = file:file_read(File), 
-%io:fwrite("~p~n",[Txt]).
+MyBucket = <<"test">>,
+
+Val1 = 1,
+
+% Obj1 = riakc_obj:new(MyBucket, <<"one">>, Val1),
+% riakc_pb_socket:put(Pid, Obj1).
+
+{ok, Fetched1} = riakc_pb_socket:get(Pid, MyBucket, <<"one">>),
+Val1 = binary_to_term(riakc_obj:get_value(Fetched1)),
+io:fwrite("~p~n", [Val1]).
+
+% erl -pa /Users/alex/Documents/hrsf110/riak-erlang-client/ebin/ /Users/alex/Documents/hrsf110/riak-erlang-client/deps/*/ebin /Users/alex/Documents/hrsf110/jsx
+% {ok, Pid} = riakc_pb_socket:start("127.0.0.1", 8087).
